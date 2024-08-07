1840c541cdcf3a7aa491b7c1139bac505afd37d5
Musse <musse.hidru@outlook.com>
2024-08-07 01:11:49 -0600

    Audio tweek

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -61,6 +61,17 @@ public class AudioManager : Singleton<AudioManager>
         return newAudioSource;
     }
 
+    public void StopBGAudio()
+    {
+        foreach (AudioSource audioSource in _audioSources)
+        {
+           if(audioSource.outputAudioMixerGroup == bgMusicGroup)
+            {
+                audioSource.Stop();
+            }
+        }
+    }
+
     public void StopAllAudio()
     {
         foreach (AudioSource audioSource in _audioSources)
@@ -72,6 +83,11 @@ public class AudioManager : Singleton<AudioManager>
     public void PlayTimeoutSound()
     {
         PlayAudio(timeoutClip);
+    }
+
+    public void MuteAudio(bool isMuted,float volume)
+    {
+        mixer.SetFloat("Master", isMuted ? Mathf.Log10(0.001f) * 20 : Mathf.Log10(volume) * 20);
     }
 
     private void OnEnable()
