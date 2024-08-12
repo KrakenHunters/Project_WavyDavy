0f645e7c63221a0253bf23f69fb6264a0a01098f
Renee Camillo Pollet <renee_p_35@hotmail.com>
2024-08-11 21:21:14 -0600

    BoardWave ANimation

---
+++ Assets/Scripts/Player/BoardWave.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BoardWave : MonoBehaviour
+{
+    public GameEvent Event;
+    [SerializeField] private Ease Ease;
+
+    [SerializeField] private GameObject FrontWave;
+    [SerializeField] private GameObject BackWave;
+
+    private Animator animator;
+    private void OnEnable()
+    {
+        Event.OnChangeGameState.AddListener(EnableWave);
+        Event.OnFlowChange.AddListener(ScaleAndSpeed);
+    }
+
+    private void OnDisable()
+    {
+        Event.OnChangeGameState.AddListener(EnableWave);
+        Event.OnFlowChange.AddListener(ScaleAndSpeed);
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void ScaleAndSpeed(float flow)
+    {
+        float newScale = Mathf.Lerp(0.6f, 2.4f, flow / 20f);
+        transform.DOScale(new Vector3(newScale, newScale, newScale), 0.5f).SetEase(Ease);
+        float newSpeed = Mathf.Lerp(0.6f, 2.4f, flow / 20f);
+        animator.speed = newSpeed;
+    }
+
+    private void EnableWave(GamePhase gamePhase)
+    {
+        switch (gamePhase)
+        {
+            case GamePhase.Trick:
+                FrontWave.SetActive(false);
+                BackWave.SetActive(false);
+                break;
+            default:
+                FrontWave.SetActive(true);
+                BackWave.SetActive(true);
+                break;
+        }
+    }
+
+
+
+}
