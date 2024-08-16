40eff3aa6bd08c0ef27f4a28aa7ab2aef0b0cbfd
Musse <musse.hidru@outlook.com>
2024-08-15 18:47:29 -0600

    Controller Navigation

--- Assets/Scripts/UI/FlowUIHandler.cs
+++ Assets/Scripts/UI/FlowUIHandler.cs
@@ -5,7 +5,6 @@ public class FlowUIHandler : MonoBehaviour
 {
     [SerializeField] private Slider flowSlider;
     [SerializeField] private float speed = 5f;
-    [SerializeField] private GameObject upArrow;
 
     private float target = 0f;
 
@@ -16,8 +15,6 @@ public class FlowUIHandler : MonoBehaviour
         flowSlider.minValue = minFlow;
         flowSlider.maxValue = maxFlow;
     }
-
-    public void ToggleArrow(bool state) => upArrow.SetActive(state);
 
     private void Update() => flowSlider.value = Mathf.Lerp(flowSlider.value, target, speed * Time.deltaTime);
 }
